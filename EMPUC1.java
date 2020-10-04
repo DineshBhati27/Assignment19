0860688dcc7e85c83762cd608443aca6ee83e2e2
DineshBhati27 <69006038+DineshBhati27@users.noreply.github.com>
2020-10-04 20:31:47 +0530

    Update EMPUC1.java

--- EMPUC1.java
+++ EMPUC1.java
@@ -3,11 +3,11 @@ class EMPUC1{
     
     public static void main(String args[]){
     
-    int is_FULL_Time=1;
+    int is_Present=1;
     Random ran=new Random();
     int empCheck=ran.nextInt(2);
     
-       if(is_FULL_Time == empCheck){
+       if(is_Present == empCheck){
              System.out.println("Employee is Present");
           }
        else
